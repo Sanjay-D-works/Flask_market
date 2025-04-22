2e89bee525960eb831708a6691a5ab510237cb29
Your Name <you@example.com>
2025-04-22 10:20:54 +0530

    Converted Flask_market from submodule to regular folder

---
+++ Flask_market/shop.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/Home')
+def home_page():
+    return render_template('Home.html')
+
+@app.route('/Market')
+def market_page():
+    item = [
+        {'id': 1,'name':'Phone','barcode':'916381008313','price':500},
+        {'id': 1, 'name': 'Laptop', 'barcode': '916381001383', 'price': 700},
+        {'id': 1, 'name': 'Keyboard', 'barcode': '916381830013', 'price': 800}
+    ]
+    return render_template('market.html',item=item)
