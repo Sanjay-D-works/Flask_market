59651515871e2befc7c223f82cfda7147a1decff
Your Name <you@example.com>
2025-04-23 22:31:24 +0530

    Models and Database

--- shop.py
+++ shop.py
@@ -1,30 +1,27 @@
 from enum import unique
-
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
 
-
-
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
-    price = db.Column(db.Integer(),nullable=False)
-    barcode = db.Column(db.String(length=12),nullable=False,unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
 
-@app.route('/')
-@app.route('/Home')
-def home_page():
-    return render_template('Home.html')
+    def __repr__(self):
+        return f'Item {self.name}'
 
-@app.route('/Market')
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
 def market_page():
-    item = [
-        {'id': 1,'name':'Phone','barcode':'916381008313','price':500},
-        {'id': 1, 'name': 'Laptop', 'barcode': '916381001383', 'price': 700},
-        {'id': 1, 'name': 'Keyboard', 'barcode': '916381830013', 'price': 800}
-    ]
-    return render_template('market.html',item=item)
+    items = Item.query.all()
+    return render_template('market.html', items=items)
