7092ef495b0c3cca87924d9ab29d687bc72ec0a2
Your Name <https://github.com/Sanjay-D-works>
2025-05-02 22:29:49 +0530

    Adding the file form.py

--- Market/__init__.py
+++ Market/__init__.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  # Replace with your DB URI
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 from Market import route
