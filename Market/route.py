4718e404afc26eef9803ac5ae88257cb85ed9114
Sanjay <sanjaydevarajworks@gmail.com>
2025-07-02 22:50:25 +0530

    Flask Validation

--- Market/route.py
+++ Market/route.py
@@ -30,4 +30,7 @@ def register_page():
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
+    if form.errors != {}: # IF there are not errors from the validations
+        for err_msg in form.errors.values():
+            print(f'There was an error with creating a user: {err_msg}')
     return render_template('register.html', form=form)
