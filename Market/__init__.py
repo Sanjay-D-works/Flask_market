aabfd772fe32906722b39fe749174a11cd9806b0
Sanjay <sanjaydevarajworks@gmail.com>
2025-07-02 22:22:11 +0530

    Upgrade the migrations file...

--- Market/__init__.py
+++ Market/__init__.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'b400e58b4acf076b17c5227e'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from Market import models  # import your models after db is defined
 from Market import route
--- Market/route.py
+++ Market/route.py
@@ -1,10 +1,14 @@
-from jupyter_server.auth import passwd
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
 
 from Market import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from Market.models import Item, User
 from Market.forms import RegisterForm
 from Market import db
+
+
+
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -15,13 +19,15 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
 
-@app.route('/register')
+#Cross Site Request Forgery(CSRF)
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                               email_address=form.email_address.data,
-                              password1=form.password1.data)
+                              password_hash=generate_password_hash(form.password1.data))
         db.session.add(user_to_create)
         db.session.commit()
+        return redirect(url_for('market_page'))
     return render_template('register.html', form=form)
